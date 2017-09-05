b6bf640413c8aca629f4ff8dd60fa2188d8670c4
mao <gunnermao@163.com>
2017-09-05 15:38:25 +0800

    add ResNet50Pre_trained.py

---
+++ ResNet50Pre_trained.py
@@ -0,0 +1,15 @@
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights='imagenet')
+
+imp_path = 'testpics/car.jpg'
+img = image.load_img(imp_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+print(decode_predictions(preds))
